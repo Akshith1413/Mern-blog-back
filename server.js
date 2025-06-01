13f17fc5964dd0e9ee8417e8443314d59fba480f
Akshith <ravulaakshith1@gmail.com>
2025-06-01 08:05:07 +0530

    Initial commit

--- server.js
+++ server.js
@@ -78,6 +78,7 @@ const commentSchema = new mongoose.Schema(
     post: { type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true },
     author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     content: { type: String, required: true },
+    
   },
   { timestamps: true }
 );
